d4643f4d1acde2169378028e30e6d63f13f6d0b1
Anthony Liu <tonyliu.30@gmail.com>
2014-06-18 14:18:49 -0400

    added first bit of viz code

---
+++ site/js/test.js
@@ -0,0 +1,54 @@
+var width = 800;
+var height = 600;
+
+var color = d3.scale.category10();
+
+var force = d3.layout.force()
+    .charge(-980)
+    .linkDistance(function(d) {return d.weight * 200;} )
+    .size([width, height]);
+
+var svg = d3.select("#graph");
+
+d3.json("./js/test3.json", function(graph) {
+    force
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .start();
+
+    var links = svg.selectAll("line.link")
+        .data(force.links())
+        .enter().append("line")
+        .attr("class", "link")
+        .attr("stroke-width", 1)
+        .attr("stroke", "#aaa");
+
+    var nodes = svg.selectAll("g.node")
+        .data(graph.nodes)
+
+    var nodeBoxs = nodes.enter().append("g")
+        .attr("class", "node");
+
+    nodeBoxs.append("circle")
+        .attr("r", 8)
+        .style("fill", function(d) {return color(d.group); });
+
+    nodeBoxs.append("text")
+        .attr("dx", 9)
+        .style("text-anchor", "start")
+        .style("font-size", "14px")
+        .style("font-color", "#000")
+        .text(function(d) { return d.name; });
+
+    console.log(nodes);
+
+    force.on("tick", function() {
+        links.attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+
+        nodes.attr("transform", function(d) { return "translate(" + [d.x, d.y] + ")"; });
+
+    });
+});
---
+++ viz.py
@@ -0,0 +1,50 @@
+from sim_score import read_mtx
+import json
+
+def mtx_to_json(ifname, ofname, cutoff):
+    def mknode(name):
+        node = {'name': name}
+        return node
+
+    def mkedge(v,w,weight):
+        edge = {'source':v, 'target':w}
+        edge['value'] = weight
+        return edge
+
+    m, hdr, hdr2 = read_mtx(ifname, transpose = False, rowname = False)
+    graph = {"nodes": [], "links": []}
+    for i in range(len(hdr)):
+        node = mknode(hdr[i])
+        graph['nodes'].append(node)
+        for j in range(i+1, len(hdr)):
+            weight = m[i,j]
+            if weight < cutoff: continue
+            else:
+                edge = mkedge(i,j,weight)
+                graph['links'].append(edge)
+    json_fmt = json.dumps(graph)
+    open(ofname, 'w').write(json_fmt)
+    return None
+
+def nx_to_json(graph, ofname):
+    def mknode(name):
+        node = {'name': name}
+        return node
+
+    def mkedge((v,w)):
+        edge = {'source': v, 'target': w}
+        data = graph.get_edge_data(v,w)
+        for key in data:
+            edge[key] = data[key]
+        return edge
+
+    nodes = map(mknode, graph.nodes_iter())
+    edges = map(mkedge, graph.edges_iter())
+
+    graph = {"nodes": nodes, "links": edges}
+
+    json_fmt = json.dumps(graph)
+    open(ofname, 'w').write(json_fmt)
+    return None
+if __name__ == '__main__':
+    mtx_to_json('testhgd.mtx', './site/js/testhgd.json', 0)
