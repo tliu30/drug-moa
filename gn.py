d0d740f025c11d80551d4b6d9a7d6c717b281147
Anthony Liu <tonyliu.30@gmail.com>
2014-06-18 21:59:05 -0400

    initial gm implementation; need to evaluate faster implementation

---
+++ gn.py
@@ -0,0 +1,59 @@
+import networkx as nx
+
+def girvan_newman(g):
+    def get_edges(paths):
+        edges = []
+        for path in paths:
+            edges += zip(path[:-1], path[1:])
+            edges += zip(path[1:] , path[:-1])
+        return set(edges)
+
+    def count_nodes(paths):
+        nodes = {}
+        for path in paths:
+            for node in path[1:-1]:
+                nodes[node] = nodes.get(node, 0) + 1
+        return nodes
+
+    def update_sp(shortest_path, sigma, sigma_per, edge):
+        to_edit = set()
+        for source in shortest_path:
+            for target in shortest_path[source]:
+                edges = get_edges(shortest_path[source][target])
+                if (source, target) in edges: 
+                    to_edit.add( (source, target) )
+
+        for (source, target) in to_edit:
+            new_paths = nx.algorithms.shortest_path(g, source, target)
+            shortest_path[source][target] = new_paths
+            sigma[source][target] = len(new_paths)
+            counts = count_nodes(new_paths)
+            for node in counts:
+                sigma_per[node][source][target] = counts[node]
+
+        return ALL
+    
+
+    def compare(a,b):
+        if len(a) == len(b):
+            return True
+    ccs = nx.algorithms.components.connected_component_subgraphs
+
+    l = [ccs(g)]
+    while len(g.edges()) != 0:
+        ebc = nx.centrality.edge_betweenness_centrality(g)
+        (v,w) = sorted(ebc.iteritems(),key=lambda x: x[1],reverse=True)[0][0]
+        g.remove_edge(v,w)
+        z = ccs(g)
+        if not compare(z, g[-1]):
+            l.append(z)
+        print len(g.edges())
+    return l
+
+if __name__ == "__main__":
+    from viz import mtx_to_nx
+    g = mtx_to_nx('randhgd.mtx', 0.5)
+    steps = girvan_newman(g)
+    for s in steps:
+        print ' '.join( [len(r.nodes()) for r in s] )
+    
